7ef8a7a61cc17c023bd9bc42787f7c2f77a57acf
Ravi Sharma <ravis4376@gmail.com>
2015-07-08 21:21:24 +0530

    added script for reading parameters.

---
+++ parameters.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "The positional parameters are: $@"
+echo "The number of pos.parameters : $#"
+echo "The 0 positional parameter is the name of sript : $0"
+echo "The 1 positional parameter is : $1"
+echo "The 2 positional parameter is : $2"
+echo "The 3 postional parameter is : $3"
+
+#bash parameters.sh arg1 arg2 arg3
