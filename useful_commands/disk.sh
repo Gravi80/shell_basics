c7fc556f112885534c321a7a26ca3df4a145637e
Ravi Sharma <ravis4376@gmail.com>
2015-07-30 19:24:42 +0530

    temp

--- useful_commands/disk.sh
+++ useful_commands/disk.sh
@@ -1,1 +1,12 @@
 mount | column -t : Lists mounted file system, in nice formatting with specification.
+
+To Find If Disk is SSD or HDD
+------------------------------
+cat /sys/block/sda/queue/rotational
+
+You should get 1 for hard disks and 0 for a SSD.
+
+
+
+
+
